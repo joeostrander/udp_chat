19328a3b49d4da902347b0ae49d8c3f464cc7ed3
Joe Ostrander <ostrander.jm@gmail.com>
2019-12-08 09:14:21 -0600

    Add project files.

---
+++ udp_chat/Program.cs
@@ -0,0 +1,91 @@
+﻿/* 
+ *  Based on code from:  https://gist.github.com/darkguy2008/413a6fea3a5b4e67e5e0d96f750088a9 
+ * 
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace udp_chat
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length < 2)
+            {
+                show_usage();
+                return;
+            }
+
+
+            string server_ip;
+            int port_server;
+            int port_client;
+
+            if (!args[0].Contains(":"))
+            {
+                show_usage();
+                Console.WriteLine("Bad server:port");
+                return;
+            }
+
+            string[] server_and_port = args[0].Split(':');
+
+            server_ip = server_and_port[0];
+
+            if (!Int32.TryParse(server_and_port[1], out port_server))
+            {
+                show_usage();
+                Console.WriteLine("Bad server port");
+                return;
+            }
+
+            if (!Int32.TryParse(args[1], out port_client))
+            {
+                show_usage();
+                Console.WriteLine("Bad listen port");
+                return;
+            }
+
+            UDPSocket s = new UDPSocket();
+            s.Server(server_ip, port_server);
+            s.debug = false;
+
+            UDPSocket c = new UDPSocket();
+            c.Client("127.0.0.1", port_client);
+            c.debug = false;
+
+            while (true)
+            {
+                var key = Console.ReadKey();
+                //Debug.WriteLine("Send key:  {0}", (int)key.KeyChar);
+                if ( key.Key == ConsoleKey.Enter)
+                {
+                    c.Send("\n");
+                    Console.WriteLine();
+                }
+                else
+                {
+                    c.Send(key.KeyChar.ToString());
+                }
+            }
+
+        }
+
+        static void show_usage()
+        {
+            Console.WriteLine("Joe Ostrander");
+            Console.WriteLine("2019.12.08");
+            Console.WriteLine();
+            Console.WriteLine("Usage:  udp_chat.exe <server:port> <listen port>");
+            Console.WriteLine();
+
+        }
+    }
+}
+
---
+++ udp_chat/UDPSocket.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace udp_chat
+{
+    public class UDPSocket
+    {
+        private Socket _socket;
+        private const int bufSize = 8 * 1024;
+        private State state = new State();
+        private EndPoint epFrom = new IPEndPoint(IPAddress.Any, 0);
+        private AsyncCallback recv = null;
+        public bool debug = false;
+
+        public class State
+        {
+            public byte[] buffer = new byte[bufSize];
+        }
+
+        public void Server(string address, int port)
+        {
+            _socket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+            _socket.SetSocketOption(SocketOptionLevel.IP, SocketOptionName.ReuseAddress, true);
+            _socket.Bind(new IPEndPoint(IPAddress.Parse(address), port));
+            Receive();
+        }
+
+        public void Client(string address, int port)
+        {
+            _socket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+            _socket.Connect(IPAddress.Parse(address), port);
+            Receive();
+        }
+
+        public void Send(string text)
+        {
+            byte[] data = Encoding.ASCII.GetBytes(text);
+            _socket.BeginSend(data, 0, data.Length, SocketFlags.None, (ar) =>
+            {
+                State so = (State)ar.AsyncState;
+                int bytes = _socket.EndSend(ar);
+                if (debug)
+                {
+                    Console.WriteLine("SEND: {0}, {1}", bytes, text);
+                }
+            }, state);
+        }
+
+        private void Receive()
+        {
+            _socket.BeginReceiveFrom(state.buffer, 0, bufSize, SocketFlags.None, ref epFrom, recv = (ar) =>
+            {
+                try
+                {
+                    State so = (State)ar.AsyncState;
+                    int bytes = _socket.EndReceiveFrom(ar, ref epFrom);
+                    if (debug)
+                    {
+                        Console.WriteLine("RECV: {0}: {1}, {2}", epFrom.ToString(), bytes, Encoding.ASCII.GetString(so.buffer, 0, bytes));
+                    }
+                    else
+                    {
+                        string received = Encoding.ASCII.GetString(so.buffer, 0, bytes);
+
+                        Console.Write(received);
+                    }
+
+                    _socket.BeginReceiveFrom(so.buffer, 0, bufSize, SocketFlags.None, ref epFrom, recv, so);
+                }
+                catch { }
+
+            }, state);
+        }
+    }
+}
